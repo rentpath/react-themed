a595165f7417bb9599dfa143674fea4729c0e026
Daniil Veremchuk <daniel@verydanny.com>
2019-08-21 17:18:24 -0400

    typings

---
+++ index.d.ts
@@ -0,0 +1,56 @@
+import * as React from 'react'
+
+interface IOptions {
+  pure?: boolean
+}
+interface IThemedProps {
+  theme: { [x: string]: string }
+}
+
+interface IThemeProviderProps {
+  theme: string | IThemedProps | IThemedProps[]
+}
+
+type composeTargetT = {}
+type themesT = Array<any>
+type composeOutputT = { [x: string]: string }
+
+type Matching<InjectedProps, DecorationTargetProps> = {
+  [P in keyof DecorationTargetProps]: P extends keyof InjectedProps
+    ? InjectedProps[P] extends DecorationTargetProps[P]
+      ? DecorationTargetProps[P]
+      : InjectedProps[P]
+    : DecorationTargetProps[P]
+}
+
+type Shared<
+  InjectedProps,
+  DecorationTargetProps extends Shared<InjectedProps, DecorationTargetProps>
+  > = {
+      [P in Extract<keyof InjectedProps, keyof DecorationTargetProps>]?: InjectedProps[P] extends DecorationTargetProps[P] ? DecorationTargetProps[P] : never
+  }
+
+type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>
+
+type GetProps<C> = C extends React.ComponentType<infer P> ? P : never
+
+type WrappedComponentClass<WrappedComponent, Props> = React.ComponentClass<JSX.LibraryManagedAttributes<WrappedComponent, Props>> & {
+  WrappedComponent: WrappedComponent
+}
+
+type InferableComponentEnhancerWithProps<InjectedProps, INeedsProps> =
+  <C extends React.ComponentType<GetProps<C>>>(component: C) =>
+    WrappedComponentClass<C, Omit<GetProps<C>, keyof Shared<InjectedProps, GetProps<C>>> & INeedsProps>
+
+type InferableComponentEnhancer<InjectedProps> = InferableComponentEnhancerWithProps<InjectedProps, {}>
+
+interface Themed {
+  (theme?: string[] | string | RegExp, options?: IOptions): InferableComponentEnhancer<IThemedProps>
+}
+
+export class ThemeProvider extends React.Component<IThemeProviderProps> {
+
+}
+export function compose(target?: composeTargetT, ...themes: themesT): composeOutputT
+export const themed: Themed
+export default themed
